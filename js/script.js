c39b141e85b41da7e09c25d860a6b48d237be868
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-12-02 22:50:18 -0500

    check if two images match

--- js/script.js
+++ js/script.js
@@ -11,11 +11,29 @@
 
 // const selectRandomImage = (images) => {};
 
+// counter number of tiles clicked
 let counter = 0;
+// track clicked tiles
+let clickedTiles = [];
 
 // list of all the tiles on the screen
 const tiles = document.getElementsByClassName("tile");
 
+// Helper functions
+function getImageName(imageUrl) {
+  return imageUrl.substring(
+    imageUrl.lastIndexOf("/"),
+    imageUrl.lastIndexOf(".")
+  );
+}
+
+/**
+ * End of Helper functions
+ */
+
+/**
+ * Main Functions
+ */
 function hideImage() {
   for (const tile of tiles) {
     if (!tile.firstElementChild.classList.contains("hide")) {
@@ -25,14 +43,28 @@ function hideImage() {
   }
 }
 
+function checkMatch(clickedTiles) {
+  const firstImageName = getImageName(clickedTiles[0].firstElementChild.src);
+  const secondImageName = getImageName(clickedTiles[1].firstElementChild.src);
+  return firstImageName.toLowerCase() === secondImageName.toLowerCase();
+}
+
+function removeTiles() {}
+
 const revealImage = (tiles) => {
   for (const tile of tiles) {
     tile.addEventListener("click", (e) => {
       counter++;
+      clickedTiles.push(tile);
       if (counter === 2) {
-        console.log(counter);
         counter = 0;
-        setTimeout(hideImage, 500);
+        // check if the images match
+        if (checkMatch(clickedTiles)) console.log("match");
+        // hide image if no match after 0.5s
+        else setTimeout(hideImage, 500);
+
+        // reset this array for the next try
+        clickedTiles = [];
       }
       e.currentTarget.firstElementChild.classList.remove("hide");
       tile.style.backgroundColor = "#fff";
